5235ec84bedea780119316a7fa6d5f4375bd12a7
AimForNaN <aimfornan@localhost>
2025-02-25 13:57:14 -0500

    Improved App test.

--- tests/NaN/App/AppTest.php
+++ tests/NaN/App/AppTest.php
@@ -8,9 +8,24 @@ use Psr\Http\Message\{
 };
 
 describe('App', function () {
-	test('Route dependency injection', function () {
+	test('Route dependency injection (closure)', function () {
 		$routes = new Router();
 		$routes['/1'] = function (PsrResponseInterface $response) {
+			return $response;
+		};
+
+		$app = new App();
+		$app->use($routes);
+
+		$rsp = $app->handle(new Request('GET', '/1'));
+		expect($rsp)->toBeInstanceOf(PsrResponseInterface::class);
+		expect($rsp->getStatusCode())->toBe(200);
+	});
+
+	test('Route param injection (closure)', function () {
+		$routes = new Router();
+		$routes['/{id}'] = function ($id, PsrResponseInterface $response) {
+			expect($id)->toBe('1');
 			return $response;
 		};
 

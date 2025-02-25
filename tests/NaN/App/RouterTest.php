33b03785ff63515c4c56f9db3110f06323552fdb
AimForNaN <aimfornan@localhost>
2025-02-25 13:18:16 -0500

    RouterTest updates.

--- tests/NaN/App/RouterTest.php
+++ tests/NaN/App/RouterTest.php
@@ -11,6 +11,38 @@ describe('Router', function () {
 		expect($route)->toBeInstanceOf(Route::class);
 	});
 
+	test('Adding routes (manually)', function () {
+		$root = new Route('/');
+		$nested_route = new Route('/nested');
+		$nested_route_route = new Route('/nested/route', function () {});
+
+		$root->insert('nested', $nested_route);
+		$nested_route->insert('route', $nested_route_route);
+
+		$routes = new Router($root);
+		$route = $routes['/nested/route'];
+
+		expect($route)->toBeInstanceOf(Route::class);
+	});
+
+	test('Adding routes (array)', function () {
+		$routes = Router::fromArray([
+			':handler' => null,
+			':path' => '/',
+			'nested' => [
+				':handler' => null,
+				':path' => '/nested',
+				'route' => [
+					':handler' => function () {},
+					':path' => '/nested/route',
+				],
+			],
+		]);
+		$route = $routes['/nested/route'];
+
+		expect($route)->toBeInstanceOf(Route::class);
+	});
+
 	/**
 	 * @todo
 	 */

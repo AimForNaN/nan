092a5b1fd26bdc1946e3e16b2de3373ce7e1fb3e
AimForNaN <aimfornan@localhost>
2025-02-27 00:50:37 -0500

    Refactoring.

--- benchmarks/CollectionBench.php
+++ benchmarks/CollectionBench.php
@@ -11,7 +11,7 @@ class CollectionBench {
 		}
 	}
 
-	public function benchFunction() {
+	public function benchCollection() {
 		$collection = new Collection();
 
 		for ($i = 0; $i < 1000; $i++) { 

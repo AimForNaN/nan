6ea51cb86b6cad8c2c8ecd26014d61bb595aac9a
AimForNaN <aimfornan@localhost>
2025-02-27 19:52:06 -0500

    Init docs.

---
+++ docs/astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import mdx from '@astrojs/mdx';
+import markdoc from '@astrojs/markdoc';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'NaN micro-framework',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Getting started',
+					autogenerate: {directory: 'getting-started'},
+				},
+				{
+					label: 'Components',
+					autogenerate: {directory: 'components'},
+				},
+			],
+		}),
+		markdoc(),
+		mdx(),
+	],
+});

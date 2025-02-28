960ffc98d6c536d6c120890e068e6d72518d276d
AimForNaN <aimfornan@localhost>
2025-02-27 23:30:10 -0500

    Refactoring.

--- src/DI/Definitions.php
+++ src/DI/Definitions.php
@@ -7,19 +7,19 @@ use NaN\DI\Interfaces\DefinitionInterface;
 class Definitions extends \NaN\Collections\TypedCollection {
 	protected mixed $type = DefinitionInterface::class;
 
-	public function get(string $alias): ?DefinitionInterface {
-		if ($this->offsetExists($alias)) {
-			return $this->offsetGet($alias);
+	public function get(string $id): ?DefinitionInterface {
+		if ($this->offsetExists($id)) {
+			return $this->offsetGet($id);
 		}
 
-		return $this->find(fn(DefinitionInterface $definition) => $definition->is($alias));
+		return $this->find(fn(DefinitionInterface $definition) => $definition->is($id));
 	}
 
-	public function has(string $alias): bool {
-		if ($this->offsetExists($alias)) {
+	public function has(string $id): bool {
+		if ($this->offsetExists($id)) {
 			return true;
 		}
 
-		return $this->some(fn(DefinitionInterface $definition) => $definition->is($alias));
+		return $this->some(fn(DefinitionInterface $definition) => $definition->is($id));
 	}
 }

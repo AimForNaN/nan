4f38a89c58fa78267df232120b23a9439a6bfae8
AimForNaN <aimfornan@localhost>
2025-03-03 09:41:44 -0500

    Update tests.

--- tests/NaN/DI/DefinitionTest.php
+++ tests/NaN/DI/DefinitionTest.php
@@ -1,9 +1,5 @@
 <?php
 
-use Monolog\{
-	Handler\ErrorLogHandler,
-	Logger,
-};
 use NaN\DI\{
 	Container,
 	Definition,
@@ -14,24 +10,15 @@ use NaN\Http\{
 };
 
 describe('Dependency Injection: Definition', function () {
-	test('Resolution with arguments', function () {
+	test('Resolution closure with arguments', function () {
 		$container = new Container(new Definitions([
 			new Definition(Request::class, ['GET', '/']),
 		]));
-		$definition = new Definition(function ($id, Request $req) {
+		$definition = new Definition(function (int $id, Request $req) {
 			expect($id)->toBe(1);
 			return $req->getMethod() === 'GET';
 		}, [1, Request::class]);
 
 		expect($definition->resolve($container))->toBeTrue();
-	});
-
-	test('Method calls', function () {
-		$container = new Container(new Definitions([
-			'logger' => (new Definition(Logger::class, ['dev_log']))->addMethodCall('pushHandler', [ErrorLogHandler::class])->setShared(),
-			(new Definition(ErrorLogHandler::class)),
-		]));
-
-		expect($container->get('logger'))->toBeInstanceOf(Logger::class);
 	});
 });

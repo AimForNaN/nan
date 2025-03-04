d56db2305acce108c003617d52406acd0553b413
AimForNaN <aimfornan@localhost>
2025-03-04 05:58:16 -0500

    Improved benchmarks.

--- benchmarks/AppBench.php
+++ benchmarks/AppBench.php
@@ -1,24 +1,41 @@
 <?php
 
 use NaN\App;
+use NaN\App\Middleware;
 use NaN\App\Middleware\Router;
 use NaN\DI\{
 	Container,
 	Definition,
 	Definitions,
 };
+use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
 
 class AppBench {
 	/**
 	 * @Iterations(10)
 	 * @Revs(10)
-	 * @Warmup(1)
 	 */
 	public function benchNanAppStartup() {
+		$app = new App();
+	}
+
+	/**
+	 * @Iterations(10)
+	 * @Revs(10)
+	 * @Warmup(1)
+	 */
+	public function benchNanAppRun() {
 		$_SERVER['REQUEST_METHOD'] = 'GET';
 		$_SERVER['REQUEST_URI'] = '/';
 
-		$app = new App();
+		$router = new Router();
+		$router['/'] = function (PsrResponseInterface $rsp) {
+			return $rsp;
+		};
+
+		$app = new App(middleware: new Middleware([
+			$router,
+		]));
 		$app->run();
 	}
 }

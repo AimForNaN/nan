e83ec3c3065db5020a062b130932eafbd1671b74
AimForNaN <aimfornan@localhost>
2025-03-08 00:07:56 -0500

    Refactoring.

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -3,23 +3,34 @@
 namespace NaN\Http;
 
 use GuzzleHttp\Psr7\Utils;
-use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
 
 class Response extends \GuzzleHttp\Psr7\Response {
 	public function __construct(int $status = 200, array $headers = [], $body = null, ?string $version = null, ?string $reason = null) {
 		if (!$version) {
-			[, $version] = \explode('/', $_SERVER['SERVER_PROTOCOL'] ?? 'HTTP/1.1');
+			[, $version] = static::parseServerProtocol();
 		}
+
 		parent::__construct($status, $headers, $body, $version, $reason);
 	}
 
-	static public function json(mixed $data) {
+	static public function json(mixed $data): PsrResponseInterface {
 		return new static(200, [
 			'Content-Type' => 'application/json',
 		], \json_encode($data));
 	}
 
-	static public function redirect(string $path, int $status = 302): ResponseInterface {
+	static protected function parseServerProtocol(): array {
+		static $parsed = null;
+
+		if ($parsed === null) {
+			$parsed = \explode('/', $_SERVER['SERVER_PROTOCOL'] ?? 'HTTP/1.1');
+		}
+
+		return $parsed;
+	}
+
+	static public function redirect(string $path, int $status = 302): PsrResponseInterface {
 		if ($status < 300 || $status > 399) {
 			$status = 302;
 		}
@@ -29,7 +40,7 @@ class Response extends \GuzzleHttp\Psr7\Response {
 		]);
 	}
 
-	static public function send(ResponseInterface $rsp) {
+	static public function send(PsrResponseInterface $rsp) {
 		static::sendHeaders($rsp);
 
 		if ($rsp->getStatusCode() !== 204) {
@@ -37,17 +48,18 @@ class Response extends \GuzzleHttp\Psr7\Response {
 		}
 	}
 
-	static public function sendBody(ResponseInterface $rsp) {
+	static public function sendBody(PsrResponseInterface $rsp) {
 		$out = Utils::streamFor(fopen('php://output', 'w'));
 		$content = (string)$rsp->getBody();
 		$out->write($content);
 	}
 
-	static public function sendHeaders(ResponseInterface $rsp) {
-		$protocol = $rsp->getProtocolVersion();
+	static public function sendHeaders(PsrResponseInterface $rsp) {
+		[$protocol] = static::parseServerProtocol();
+		$version = $rsp->getProtocolVersion();
 		$status = $rsp->getStatusCode();
 		$phrase = $rsp->getReasonPhrase();
-		\header("HTTP/{$protocol} {$status} {$phrase}");
+		\header("{$protocol}/{$version} {$status} {$phrase}");
 
 		$headers = $rsp->getHeaders();
 		foreach ($headers as $name => $value) {
